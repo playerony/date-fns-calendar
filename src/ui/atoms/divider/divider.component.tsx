982b3a2304a5cee3990a73f827227b01ee71482e
playerony <pawel.wojtasinski.1995@gmail.com>
2022-01-22 00:05:48 +0100

    chore: update

--- src/ui/atoms/divider/divider.component.tsx
+++ src/ui/atoms/divider/divider.component.tsx
@@ -1,10 +1,14 @@
+import { memo } from 'react';
 import classnames from 'classnames';
 
 import { IDividerProps } from './divider.types';
 
 import './divider.styles.scss';
 
-export const Divider = ({ className, orientation = 'horizontal' }: IDividerProps): JSX.Element => (
+const DividerComponent = ({
+  className,
+  orientation = 'horizontal',
+}: IDividerProps): JSX.Element => (
   <div
     className={classnames(
       'divider-wrapper',
@@ -13,3 +17,5 @@ export const Divider = ({ className, orientation = 'horizontal' }: IDividerProps
     )}
   />
 );
+
+export const Divider = memo(DividerComponent);
