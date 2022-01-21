ac3fe420aade996ef8ee690e7faad14c42efd597
playerony <pawel.wojtasinski.1995@gmail.com>
2022-01-22 00:12:17 +0100

    chore: update

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { Children } from 'react';
 import { getDate } from 'date-fns';
 
 import { Paper, Divider, Typography } from '@ui';
@@ -6,6 +7,7 @@ import { generateMonth } from '@utils';
 
 export const App = () => {
   const currentMonth = generateMonth();
+  console.log(currentMonth);
 
   return (
     <div
@@ -16,18 +18,22 @@ export const App = () => {
         gap: '0.5em',
       }}
     >
-      {currentMonth.map((_currentWeek) => (
-        <>
-          {_currentWeek.map((_currentDay) => (
-            <Paper shadow="sm" padding="sm" style={{ height: '15em' }}>
-              <Typography as="h2" color="primary">
-                {getDate(_currentDay)}
-              </Typography>
-              <Divider />
-            </Paper>
-          ))}
-        </>
-      ))}
+      {Children.toArray(
+        currentMonth.map((_currentWeek) => (
+          <>
+            {Children.toArray(
+              _currentWeek.map((_currentDay) => (
+                <Paper shadow="sm" padding="sm" style={{ height: '15em' }}>
+                  <Typography as="h2" color="primary">
+                    {getDate(_currentDay)}
+                  </Typography>
+                  <Divider />
+                </Paper>
+              )),
+            )}
+          </>
+        )),
+      )}
     </div>
   );
 };
