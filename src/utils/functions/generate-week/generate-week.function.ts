375bfb455c0f75e6933c3702da11aa0adfae6041
playerony <pawel.wojtasinski.1995@gmail.com>
2022-01-21 22:16:42 +0100

    chore: update

--- src/utils/functions/generate-week/generate-week.function.ts
+++ src/utils/functions/generate-week/generate-week.function.ts
@@ -7,7 +7,8 @@ export const generateWeek = (date: Date = new Date()) => {
     return [];
   }
 
-  const todayStartDate = addMinutes(startOfDay(date), date.getTimezoneOffset());
+  console.log(date.getTimezoneOffset());
+  const todayStartDate = addMinutes(startOfDay(date), date.getTimezoneOffset() * 60 * 1000);
   const weekStartDate = startOfWeek(todayStartDate, { weekStartsOn: 2 });
 
   return [...Array.from({ length: 7 })].map((_, _index) => addDays(weekStartDate, _index));
