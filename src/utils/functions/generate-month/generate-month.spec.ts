8a6727c6edbc44ab68707da54b7368d7e3e60603
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-01-22 02:01:48 +0400

    chore: create generate-month function (#3)

---
+++ src/utils/functions/generate-month/generate-month.spec.ts
@@ -0,0 +1,30 @@
+import { functionImportTest } from '@utils';
+
+import { generateMonth } from './generate-month.function';
+
+describe('generateMonth function', () => {
+  functionImportTest(generateMonth);
+
+  it('should return empty array when passed date is not type of date', () => {
+    // @ts-expect-error
+    expect(generateMonth(1)).toEqual([]);
+    // @ts-expect-error
+    expect(generateMonth('213')).toEqual([]);
+    // @ts-expect-error
+    expect(generateMonth('20-11-2019')).toEqual([]);
+    // @ts-expect-error
+    expect(generateMonth([])).toEqual([]);
+  });
+
+  it('should generate full month of dates', () => {
+    const result = generateMonth(new Date(2021, 9, 10));
+
+    expect(result).toHaveLength(6);
+    expect(result[0]).toHaveLength(7);
+    expect(result[1]).toHaveLength(7);
+    expect(result[2]).toHaveLength(7);
+    expect(result[3]).toHaveLength(7);
+    expect(result[4]).toHaveLength(7);
+    expect(result[5]).toHaveLength(7);
+  });
+});
