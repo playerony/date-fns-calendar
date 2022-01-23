dd326ff9c861330114bf08c772d1b53258582bc8
playerony <pawel.wojtasinski.1995@gmail.com>
2022-01-23 15:55:17 +0100

    chore: update demo

--- src/domain/dashboard/dashboard.component.tsx
+++ src/domain/dashboard/dashboard.component.tsx
@@ -1,23 +1,58 @@
 import { addDays, subDays } from 'date-fns';
 
-import { Calendar } from '@ui';
+import { Calendar, Typography } from '@ui';
 
 const currentDate = new Date();
 
+const events = [
+  { title: 'John Bon', start: currentDate },
+  { title: 'John Blzerian', start: currentDate },
+  { title: 'John Smith', start: subDays(currentDate, 3) },
+  { title: 'Chuck Norris', start: addDays(currentDate, 5) },
+  { title: 'Chuck Norris', start: addDays(currentDate, 3) },
+  { title: 'Chuck Norris', start: addDays(currentDate, -4) },
+];
+
 export const Dashboard = () => (
-  <Calendar
-    events={[
-      { title: 'John Bon', start: currentDate },
-      { title: 'John Blzerian', start: currentDate },
-      { title: 'John Smith', start: subDays(currentDate, 3) },
-      { title: 'Chuck Norris', start: addDays(currentDate, 5) },
-      { title: 'Chuck Norris', start: addDays(currentDate, 3) },
-      { title: 'Chuck Norris', start: addDays(currentDate, -4) },
-    ]}
-    mode="week"
-    displayWeekends
-    enableDateSelection
-    onCalendarEventClick={(event) => console.log('onCalendarEventClick', event)}
-    onSelectedDatesChange={(selectedDates) => console.log('onSelectedDatesChange', selectedDates)}
-  />
+  <>
+    <div>
+      <Typography as="h1">Month calendar</Typography>
+      <Calendar
+        mode="month"
+        events={events}
+        displayWeekends
+        enableDateSelection
+        onCalendarEventClick={(event) => console.log('onCalendarEventClick', event)}
+        onSelectedDatesChange={(selectedDates) =>
+          console.log('onSelectedDatesChange', selectedDates)
+        }
+      />
+    </div>
+    <div>
+      <Typography as="h1">Month calendar without weekends and date selection</Typography>
+      <Calendar
+        mode="month"
+        events={events}
+        displayWeekends={false}
+        enableDateSelection={false}
+        onCalendarEventClick={(event) => console.log('onCalendarEventClick', event)}
+        onSelectedDatesChange={(selectedDates) =>
+          console.log('onSelectedDatesChange', selectedDates)
+        }
+      />
+    </div>
+    <div>
+      <Typography as="h1">Week calendar</Typography>
+      <Calendar
+        mode="week"
+        events={events}
+        displayWeekends
+        enableDateSelection
+        onCalendarEventClick={(event) => console.log('onCalendarEventClick', event)}
+        onSelectedDatesChange={(selectedDates) =>
+          console.log('onSelectedDatesChange', selectedDates)
+        }
+      />
+    </div>
+  </>
 );
